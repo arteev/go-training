d905eba4022758aad735095f8e25e59205734ef5
Arteev Aleksey <alarteev@yandex.ru>
2019-05-29 19:42:45 +0500

    cgo: call C from GO

---
+++ cgo/from_c/simple.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "simple.h"
+
+
+void print(char* s) {
+    printf("%s\n", s);
+}
+
+int inc(int y) {
+    return y+1;
+}
---
+++ cgo/from_c/simple.h
@@ -0,0 +1,7 @@
+#ifndef SAMPLE_H
+#define SAMPLE_H 
+
+int inc(int y);
+void print(char* s);
+
+#endif
